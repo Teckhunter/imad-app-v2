940da7d038b8e9ee0dae33ee337b3c12f1e4bfd5
Teckhunter <rahul.agrawal8752@gmail.com>
2017-03-24 02:13:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 app.get('/article-one',function(req,res){
-res.send(createTemmplate(articleOne));
+res.send(createTemplate(articleOne));
 });	
 app.get('/article-two',function(req,res){
 	res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
