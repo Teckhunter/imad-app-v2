cfcd556b7dc2534f2004832a7016e093254e540e
Teckhunter <rahul.agrawal8752@gmail.com>
2017-03-24 01:47:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,59 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var articleOne = {
+    title: 'article-one | Rahul',
+    heading: 'Article One',
+    date: '117-Mar-2017',
+    content:`<p>this article will become the base of a stupid foundation of the conglomerate and the jibber jabber
+            </p>
+            <p>this article will become the base of a stupid foundation of the conglomerate and the jibber jabber
+            </p>
+            <p>this article will become the base of a stupid foundation of the conglomerate and the jibber jabber
+            </p>
+            <p>this article will become the base of a stupid foundation of the conglomerate and the jibber jabber
+            </p>`
+    
+};
+
+function createTemplate(data){
+var title = data.title;
+var heading= data.heading;
+var content= data.content;
+var htmlTemplate= `
+    
+<html>
+    <head>
+        <title>
+        ${title}
+        </title>
+        <meta name="viewport" content="width=device-width intitial-scale=1"/>
+         <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        
+        <div class="container">
+            <div>
+                <a href="/">Home</a>
+            </div>
+            <hr>
+            <div>
+            <h3>
+               ${heading}
+            </h3>
+            </div>
+            <div>
+            ${content}
+        </div>
+    <div>
+        
+    maximus decimus and the hoppers.</div>
+    </body>
+</html>
+`;
+return htmlTemplate;
+}
+
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -13,7 +66,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 app.get('/article-one',function(req,res){
-res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+res.sendFile(createTemmplate(articleOne));
 });	
 app.get('/article-two',function(req,res){
 	res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
