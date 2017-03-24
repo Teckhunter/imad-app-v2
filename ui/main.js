9f37eb2ff15a7ed3444bd9759718ac9d12f0ffb8
Teckhunter <rahul.agrawal8752@gmail.com>
2017-03-24 20:47:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,14 @@ var element=document.getElementById('main-text');
 element.innerHTML = 'Voracious Reader';
 
 var imag = document.getElementById('madi');
+function moveRight()
+{
+    marginleft = marginleft+10;
+    img.style.marginLeft = marginLeft + 'px';
+}
+
 imag.onclick = function()
 {
+    var interval = serInterval(moveRight,100);
     imag.style.marginleft = '200px';
 };
