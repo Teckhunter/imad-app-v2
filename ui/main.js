0b0321b1405b3a2654d3a22ea24e47d02038be74
Teckhunter <rahul.agrawal8752@gmail.com>
2017-03-25 00:35:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ var button = document.getElementById('counter');
 button.onclick = function()
 {
     counter = counter+2;
-    var span = document.getElementById('count').innerHTML;
+    var span = document.getElementById('kratos').innerHTML;
     span.innerHTML = counter.toString();
     
 }
