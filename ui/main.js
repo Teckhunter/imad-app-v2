002abb3c359edf430ae952fc2786c8ecb3e01767
Teckhunter <rahul.agrawal8752@gmail.com>
2017-03-25 01:03:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,8 +3,22 @@
 var button = document.getElementById('counter');
 button.onclick = function()
 {
-    counter = counter+2;
+    
+    //making a request variable
+    var request = new XMLHttpRequest();
+
+//
+request.onreadystatechange = function() {
+    if (request.readyState === XMLHttpRequest.DONE) {
+        //Take some action
+        if (request.status === 200) {
+            var counter = request.responseText;
     var span = document.getElementById('kratos').innerHTML;
     span.innerHTML = counter.toString();
     
 }
+}
+};
+request.open('GET', 'http://teckhunter.imad.hasura-app.io/counter',true);
+request.send(null);
+};
