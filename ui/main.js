0188ebcf64819244162f4e821f8e773e5dfe1a8f
Teckhunter <rahul.agrawal8752@gmail.com>
2017-03-26 00:16:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,13 +6,25 @@ var button = document.getElementById('counter');
 button.onclick = function()
 {
 //Make a request
+    var request = new XMLHttpRequest();
 
 //Capture Response
 
+    request.onreadystatechange = function()
+    {
+        if(request.readyState === XMLHttpRequest.DONE)
+    //Take some action
+    {
+        if (request.status === 200)
+        {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                spam.innerHTML = counter.toString();
+        }
+    }
+    
+};
 
 //render variable in correct span
 
-counter= counter +1;
-var span = document.getElementById('count');
-span.innerHTML = counter.toString();
 };
