3ac0e256b902ae9b6a597ca436ddf93c9d2d6512
Teckhunter <rahul.agrawal8752@gmail.com>
2017-03-26 00:33:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,19 +13,18 @@ button.onclick = function()
     request.onreadystatechange = function()
     {
         if(request.readyState === XMLHttpRequest.DONE)
+        {
     //Take some action
-    {
         if (request.status === 200)
         {
                 var counter = request.responseText;
                 var span = document.getElementById('count');
-                spam.innerHTML = counter.toString();
+                span.innerHTML = counter.toString();
         }
-    }
-    
+        }    
 };
 //make the request
-request.open('GET','http://teckhunter.imad.hasura-app.io/',true);
+request.open('GET','http://teckhunter.imad.hasura-app.io/counter',true);
 request.send(null);
 //render variable in correct span
 
